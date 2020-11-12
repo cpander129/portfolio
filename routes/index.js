012e55b5394e8370e96e323833db17e4997849f2
cpander129 <53307367+cpander129@users.noreply.github.com>
2020-11-12 13:33:33 -0500

    Add routes and bootstrap

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET about page */
+router.get('/about', (req, res, next) => {
+  res.render('about', { title: 'About Us'});
+});
+
+/* GET projects page */
+router.get('/projects', (req, res, next) => {
+  res.render('projects', { title: 'Projects' });
+});
+
+/* GET services page */
+router.get('/services', (req, res, next) => {
+  res.render('services', { title: 'Services' });
+});
+
+/* GET contact page */
+router.get('/contact', (req, res, next) => {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+//Make file public
 module.exports = router;
